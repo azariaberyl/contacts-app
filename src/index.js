109f2d78ad46a9e05f9d281f3f76a2c2df693b8f
azariaberyl <beryl.naga@gmail.com>
2022-09-26 14:56:42 +0700

    Creating Components and styling Components then import it as module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-import { getData } from "./data";
+import ContactApp from "./components/ContactApp";
 
-const hello = <h2>Hello Wolrd</h2>
+import './Styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(hello);
+root.render(<ContactApp/>);
