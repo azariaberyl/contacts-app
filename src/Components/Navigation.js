88fbcffe051f65e203866428fb8ac98d7bc1be29
azariaberyl <beryl.naga@gmail.com>
2022-10-06 14:04:04 +0700

    Login/Register Auth

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {FiHome, FiPlusCircle, FiLogOut} from 'react-icons/fi';
 
-function Navigation() {
+function Navigation({logout, name}) {
   return (
     <nav className="navigation">
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/add">Add</Link></li>
+        <li><Link to="/"><FiHome /></Link></li>
+        <li><Link to="/add"><FiPlusCircle /></Link></li>
+        <li><button onClick={logout}>{name}<FiLogOut /></button></li>
       </ul>
     </nav>
   );
